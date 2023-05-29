85a83424f07c63bf2fccca22fb849fa30a72a844
Valki11 <keilaramirez4249@gmail.com>
2023-05-28 22:02:15 -0600

    creamos las validaciones :)

---
+++ src/main/java/com/mycompany/proyectofinalg2/Validaciones.java
@@ -0,0 +1,52 @@
+
+package com.mycompany.proyectofinalg2;
+public class Validaciones {
+    
+    public boolean ValidarFecha(String Fecha ){
+      
+            
+    return true;
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
