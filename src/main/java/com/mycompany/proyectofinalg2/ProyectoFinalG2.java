e3676b14247305d9a346a83df649a20cfb00e311
PLAlvarez11 <106171305+PLAlvarez11@users.noreply.github.com>
2023-05-28 21:40:35 -0600

    Creación Clase Vehículo
    :)

--- src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
+++ src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyectofinalg2;
 
-/**
- *
- * @author pluis
- */
 public class ProyectoFinalG2 {
 
     public static void main(String[] args) {
