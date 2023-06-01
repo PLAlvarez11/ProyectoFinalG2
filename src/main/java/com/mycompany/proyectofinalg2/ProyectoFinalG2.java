1f12a69be5322b8ba9cfa2f2a8c8ca79bf2133dd
MelanyDayRSAI <117334084+MelanyDayRSAI@users.noreply.github.com>
2023-05-31 22:35:41 -0600

    Update ProyectoFinalG2.java

--- src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
+++ src/main/java/com/mycompany/proyectofinalg2/ProyectoFinalG2.java
@@ -1,8 +1,16 @@
 package com.mycompany.proyectofinalg2;
-
+import java.util.Scanner;
 public class ProyectoFinalG2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+		System.out.println(" *   *   *   *   *   *   *   *");
+		System.out.println("   *   *  BIENVENIDO   *   *  ");
+		System.out.println(" *   *   *   *   *   *   *   *");
+		System.out.println("\n");
+		System.out.println("      MENU DEL PROYECTO G2    ");
+		System.out.println("Ingrese clientes-------------1");
+		System.out.println("Datos de clientes------------2");
+		System.out.println("Datos de vehiculos-----------3");
     }
 }
